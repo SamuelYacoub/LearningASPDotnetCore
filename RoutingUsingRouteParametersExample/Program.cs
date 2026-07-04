767c7ff65b66467a8cc7d10a01d402ce34652802
Samuel Yacoub <samuel.yacoubb@gmail.com>
2026-07-04 13:30:10 +0300

    adding if else condtions for routing with optional parameters

--- RoutingUsingRouteParametersExample/Program.cs
+++ RoutingUsingRouteParametersExample/Program.cs
@@ -15,15 +15,15 @@ namespace RoutingUsingRouteParametersExample
 
                 string? fileName = Convert.ToString(context.Request.RouteValues["filename"]);
                 await context.Response.WriteAsync($"we recive the request at products - {category} with id {id}");
-            
+
             });
 
 
-            app.MapGet("library/{category}/{filename}.{extension=kharah}",async (context) =>
+            app.MapGet("library/{category}/{filename}.{extension=kharah}", async (context) =>
             {
-                string? category     = Convert.ToString(context.Request.RouteValues["category"]);
-                string? fileName     = Convert.ToString(context.Request.RouteValues["filename"]);
-                string? extension    = Convert.ToString(context.Request.RouteValues["extension"]);
+                string? category = Convert.ToString(context.Request.RouteValues["category"]);
+                string? fileName = Convert.ToString(context.Request.RouteValues["filename"]);
+                string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
 
                 await context.Response.WriteAsync($"we recive the request of file from our library from - {category} " +
                     $"with name {fileName} and extension of file is {extension}");
@@ -53,7 +53,7 @@ namespace RoutingUsingRouteParametersExample
             {
                 int id = Convert.ToInt32(context.Request.RouteValues["id"]);
                 await context.Response.WriteAsync($"ok sir we will get the details of product id = {id}");
-                
+
             });
             app.Map("/school/{grade}/{student?}", async (context) =>
             {
@@ -62,6 +62,20 @@ namespace RoutingUsingRouteParametersExample
 
                 await context.Response.WriteAsync($"the in the school and the student whose name is {student} his grade is {grade} ");
 
+            });
+
+            app.Map("/hospital/{patient}/{id?}", async (context) =>
+            {
+                if (context.Request.RouteValues.ContainsKey("id"))
+                {
+                    int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+                    string? patient = Convert.ToString(context.Request.RouteValues["patient"]);
+                    await context.Response.WriteAsync($"in our hospital the patient {patient} with id {id} his health is verygood ");
+
+
+                }
+                else
+                    await context.Response.WriteAsync("you have missed the id of the patient ");
             });
 
             app.MapFallback(async (context) =>
