e193cd4ba70033f882b25e5059722b23423bdc45
Samuel Yacoub <samuel.yacoubb@gmail.com>
2026-08-05 22:15:29 +0300

    Controller with multiple Action methods example

---
+++ aspnet_pro024_ControllerWithMultipleActions/Controllers/MyHomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace aspnet_pro024_ControllerWithMultipleActions.Controllers
+{
+    [Controller]
+    [Route("home")]
+    public class MyHomeController
+    {
+
+        [Route("action1")]
+        public string Action1()
+        {
+            return "welcome to action 1";
+
+        }
+
+        [Route("action2")]
+
+        public string Action2()
+        {
+            return "welcome to action 2";
+
+        }
+
+        [Route("action3")]
+        public string Action3()
+        {
+            return "welcome to action 3";
+
+        }
+
+        [Route("contact/{phone:regex(^\\d{{10}}$)}")]
+        public string Contact(string phone)
+    {
+        return $"You can contact us on phone : {phone}";
+
+    }
+
+
+}
+}
---
+++ aspnet_pro024_ControllerWithMultipleActions/Program.cs
@@ -0,0 +1,17 @@
+namespace aspnet_pro024_ControllerWithMultipleActions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            var app = builder.Build();
+            app.MapControllers();
+
+      
+
+            app.Run();
+        }
+    }
+}
