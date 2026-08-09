536f9b8a1c47cd70e2128a96ec2e1f121c8f0df0
Samuel Yacoub <samuel.yacoubb@gmail.com>
2026-08-09 22:56:12 +0300

    adding Action restult project

---
+++ aspnet_pro028_IActionResultAsActionRestult/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_pro028_IActionResultAsActionRestult.Controllers
+{
+    [Controller]
+    public class HomeController
+    {
+
+        // [Route("")]
+        public IActionResult Index()
+        {
+
+            return new ContentResult() { Content = "sasa", ContentType = "text/plain" };
+
+
+        }
+
+        [Route("")]
+        [Route("filedownload1")]
+        public IActionResult FileDownload1()
+        {
+
+            return new VirtualFileResult("english.pdf", "application/pdf");
+
+
+        }
+
+        [Route("filedownload2")]
+
+        public IActionResult FileDownload2()
+        {
+
+            return new PhysicalFileResult(@"D:\projects\asp.net_core_projects\aspnet_pro027_FileResultAsActionResult\wwwroot\english.pdf", "application/pdf");
+
+
+        }
+        [Route("filedownload3")]
+
+        public IActionResult FileDownload3()
+        {
+
+            byte[] bytes = System.IO.File.ReadAllBytes(@"D:\projects\asp.net_core_projects\aspnet_pro027_FileResultAsActionResult\wwwroot\english.pdf");
+            return new FileContentResult(bytes, "application/pdf");
+
+        }
+    }
+ }
